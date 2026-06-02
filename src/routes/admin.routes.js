1b1dbb80a213198583e92aa84d4d98ae0110a242
Atharva Farkade <atharva.farkade@technogise.com>
2026-06-02 17:18:11 +0530

    refactor: replace redundant role middlewares with authorizeRoles

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
-import { verifyJwt} from "../middlewares/auth.middleware.js";
-import { checkUserRole, removeUser, updateFlightStatus } from "../controllers/admin.controller.js";
-import { verifyAdminAccess } from "../middlewares/admin.middleware.js";
-import {registerFlightDetails} from "../controllers/admin.controller.js";
-import {getAllFlights, getAllUsers } from "../controllers/admin.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/authorizeRoles.middleware.js";
+import { checkUserRole, removeUser, updateFlightStatus, getTurnaroundSummary } from "../controllers/admin.controller.js";
+import { registerFlightDetails } from "../controllers/admin.controller.js";
+import { getAllFlights, getAllUsers } from "../controllers/admin.controller.js";
 
 const adminrouter = Router()
 
-adminrouter.route("/").post(verifyJwt,verifyAdminAccess, checkUserRole);
-adminrouter.route("/flight-details").post(verifyJwt,verifyAdminAccess, registerFlightDetails);
-adminrouter.route("/upcoming-flights").get(verifyJwt,verifyAdminAccess, getAllFlights);
-adminrouter.route("/users").get(verifyJwt,verifyAdminAccess, getAllUsers);
-adminrouter.route("/remove-user").delete(verifyJwt,verifyAdminAccess, removeUser);
-adminrouter.route("/flight/:flightId/status").patch(verifyJwt,verifyAdminAccess, updateFlightStatus);
+adminrouter.route("/").post(verifyJwt, authorizeRoles("airport_admin"), checkUserRole);
+adminrouter.route("/flight-details").post(verifyJwt, authorizeRoles("airport_admin"), registerFlightDetails);
+adminrouter.route("/upcoming-flights").get(verifyJwt, authorizeRoles("airport_admin"), getAllFlights);
+adminrouter.route("/users").get(verifyJwt, authorizeRoles("airport_admin"), getAllUsers);
+adminrouter.route("/remove-user").delete(verifyJwt, authorizeRoles("airport_admin"), removeUser);
+adminrouter.route("/flight/:flightId/status").patch(verifyJwt, authorizeRoles("airport_admin"), updateFlightStatus);
+adminrouter.route("/flight/:flightId/turnaround").get(verifyJwt, authorizeRoles("airport_admin"), getTurnaroundSummary);
 
 export default adminrouter;
--- src/routes/staff.routes.js
+++ src/routes/staff.routes.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
-import { verifyJwt} from "../middlewares/auth.middleware.js";
-import { verifyStaffAccess } from "../middlewares/staff.middleware.js";
-import { getAirplanes } from "../controllers/airplane.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/authorizeRoles.middleware.js";
-import { requestServices } from "../controllers/service.controller.js";
-import { grantPermission } from "../controllers/service.controller.js";
+import {
+    requestServices,
+    grantPermission,
+    getFlightServices,
+    verifyService
+} from "../controllers/service.controller.js";
 import { getArrivedFlights } from "../controllers/user.controller.js";
 
 const staffrouter = Router()
 
-staffrouter.route("/").post(verifyJwt,verifyStaffAccess,grantPermission);
+staffrouter.route("/").post(verifyJwt, authorizeRoles("airline_staff"), grantPermission);
 staffrouter.get("/flight-details", verifyJwt, authorizeRoles("airline_staff"), getArrivedFlights);
 staffrouter.post("/service-request", verifyJwt, authorizeRoles("airline_staff"), requestServices);
+staffrouter.get("/flight/:flightId/services", verifyJwt, authorizeRoles("airline_staff"), getFlightServices);
+staffrouter.patch("/service/:serviceRequestId/verify", verifyJwt, authorizeRoles("airline_staff"), verifyService);
 
 export default staffrouter;
